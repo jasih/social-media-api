260880c9407dfe83cae8e634effde3f23ae82a2e
Janet Sihapanya <janetsihapanya@gmail.com>
2022-10-03 17:53:46 -0500

    Inital setup for service, serviceimpl, exceptions, repositories, and embeddables

---
+++ src/main/java/com/cookysys/social_media_project/embeddables/ProfileEmbeddable.java
@@ -0,0 +1,5 @@
+package com.cookysys.social_media_project.embeddables;
+
+public class ProfileEmbeddable {
+
+}
---
+++ src/main/java/com/cookysys/social_media_project/services/impl/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.cookysys.social_media_project.services.impl;
+
+public class UserServiceImpl {
+
+}

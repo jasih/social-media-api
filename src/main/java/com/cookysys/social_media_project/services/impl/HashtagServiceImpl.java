7b6807e2a5c2db57bf813f1b9eb318a6f1bdd18b
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 10:47:41 -0500

    Update HashtagServiceImpl.java

--- src/main/java/com/cookysys/social_media_project/services/impl/HashtagServiceImpl.java
+++ src/main/java/com/cookysys/social_media_project/services/impl/HashtagServiceImpl.java
@@ -2,6 +2,6 @@ package com.cookysys.social_media_project.services.impl;
 
 @Service
 @RequiredArgsConstructor
-public class HashtagServiceImpl implements HashTagService {
+public class HashtagServiceImpl implements HashtagService {
 
 }

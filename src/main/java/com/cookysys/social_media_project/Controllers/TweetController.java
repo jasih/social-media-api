a1e65395b316c7d6addb95fe878449e59500e8d3
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 10:29:20 -0500

    Update TweetController.java

--- src/main/java/com/cookysys/social_media_project/Controllers/TweetController.java
+++ src/main/java/com/cookysys/social_media_project/Controllers/TweetController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/tweet")
+@RequestMapping("/tweets")
+@RequiredArgsConstructor
 public class TweetController {
 
 }

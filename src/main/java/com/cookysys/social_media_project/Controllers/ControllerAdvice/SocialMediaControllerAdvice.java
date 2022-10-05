0ee8f07d21ff5f25194de60d906a6536694139ca
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 10:27:48 -0500

    Update SocialMediaControllerAdvice.java

--- src/main/java/com/cookysys/social_media_project/Controllers/ControllerAdvice/SocialMediaControllerAdvice.java
+++ src/main/java/com/cookysys/social_media_project/Controllers/ControllerAdvice/SocialMediaControllerAdvice.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 public class SocialMediaControllerAdvice {
 
 //	@ExceptionHandler(BadRequestException.class)
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
 //	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
 //		return new ErrorDto(badRequestException.getMessage());
 //	}

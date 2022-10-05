3327a6bb802bc865462fabea93fce6d3c77d57cc
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-10-05 10:31:43 -0500

    Update TweetMapper.java

--- src/main/java/com/cookysys/social_media_project/Mappers/TweetMapper.java
+++ src/main/java/com/cookysys/social_media_project/Mappers/TweetMapper.java
@@ -2,7 +2,7 @@ package com.cookysys.social_media_project.Mappers;
 
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { UserMapper.class })
 public interface TweetMapper {
     
 }

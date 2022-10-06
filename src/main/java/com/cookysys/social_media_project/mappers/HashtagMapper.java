15622b12b1997e583fc533e253da8bca05617b03
Janet Sihapanya <janetsihapanya@gmail.com>
2022-10-06 00:12:05 -0500

    Added APIs and some implementations for hashtag

--- src/main/java/com/cookysys/social_media_project/mappers/HashtagMapper.java
+++ src/main/java/com/cookysys/social_media_project/mappers/HashtagMapper.java
@@ -1,8 +1,18 @@
 package com.cookysys.social_media_project.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+
+import com.cookysys.social_media_project.dtos.HashtagDto;
+import com.cookysys.social_media_project.dtos.TweetResponseDto;
+import com.cookysys.social_media_project.entities.Hashtag;
 
 @Mapper(componentModel = "spring")
 public interface HashtagMapper {
+
+	List<HashtagDto> entitiesToDtos(List<Hashtag> findAll);
+
+	List<TweetResponseDto> entityToDto(Hashtag hashtag);
     
 }
--- src/main/java/com/cookysys/social_media_project/repositories/HashtagRepository.java
+++ src/main/java/com/cookysys/social_media_project/repositories/HashtagRepository.java
@@ -1,5 +1,7 @@
 package com.cookysys.social_media_project.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.cookysys.social_media_project.entities.Hashtag;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long>{
+
+	Optional<Hashtag> findByLabel(String label);
 
 }

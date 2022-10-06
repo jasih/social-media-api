26c413312cc967f76a8341cbef4a268d409be787
Dylan Cypert <dylancypert@gmail.com>
2022-10-05 22:13:30 -0500

    Added Twitter APIs to Controllers and Services

--- src/main/java/com/cookysys/social_media_project/mappers/TweetMapper.java
+++ src/main/java/com/cookysys/social_media_project/mappers/TweetMapper.java
@@ -1,8 +1,20 @@
 package com.cookysys.social_media_project.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+
+import com.cookysys.social_media_project.dtos.TweetRequestDto;
+import com.cookysys.social_media_project.dtos.TweetResponseDto;
+import com.cookysys.social_media_project.entities.Tweet;
 
 @Mapper(componentModel = "spring", uses = { UserMapper.class })
 public interface TweetMapper {
+
+    TweetResponseDto entityToDto(Tweet tweet);
+
+    List<TweetResponseDto> entitiesToDtos(List<Tweet> tweets);
     
+    Tweet dtoToEntity(TweetRequestDto tweetRequestDto);
+
 }

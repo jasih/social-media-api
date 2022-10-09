dd46c871d6c3d7e2c4c9e7280e2243df3f35d626
Dylan Cypert <dylancypert@gmail.com>
2022-10-09 13:31:45 -0500

    Merge branch 'Dylan' of https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-9-2022-team-2 into Dylan

--- src/main/java/com/cookysys/social_media_project/services/impl/ValidateServiceImpl.java
+++ src/main/java/com/cookysys/social_media_project/services/impl/ValidateServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cookysys.social_media_project.services.impl;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,9 +11,14 @@ import org.springframework.stereotype.Service;
 
 import com.cookysys.social_media_project.entities.Hashtag;
 import com.cookysys.social_media_project.entities.Tweet;
-import com.cookysys.social_media_project.exceptions.NotFoundException;
+import com.cookysys.social_media_project.entities.User;
 import com.cookysys.social_media_project.mappers.HashtagMapper;
 import com.cookysys.social_media_project.repositories.HashtagRepository;
+import com.cookysys.social_media_project.repositories.UserRepository;
+import com.cookysys.social_media_project.dtos.CredentialsDto;
+import com.cookysys.social_media_project.exceptions.BadRequestException;
+import com.cookysys.social_media_project.exceptions.NotAuthorizedException;
+import com.cookysys.social_media_project.exceptions.NotFoundException;
 import com.cookysys.social_media_project.services.ValidateService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,17 +28,30 @@ import lombok.RequiredArgsConstructor;
 public class ValidateServiceImpl implements ValidateService  {
 	
 	private final HashtagRepository hashtagRepository;
+	private final UserRepository userRepository;
 	
 	@Override
 	public boolean userNameExists(String username) {
-		// TODO Auto-generated method stub
-		return false;
+		Optional<User> optionalUser = userRepository.findByCredentialsUsernameAndDeletedFalse(username);
+		if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getCredentials().getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
 	}
 
 	@Override
 	public boolean userNameAvailable(String username) {
-		// TODO Auto-generated method stub
-		return false;
+		Optional<User> optionalUser = userRepository.findByCredentialsUsernameAndDeletedFalse(username);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getCredentials().getUsername().equals(username)) {
+                throw new BadRequestException("A user already exists with the username : " + username);
+            }
+        }
+        return true;
 	}
 	
 	
@@ -49,5 +68,14 @@ public class ValidateServiceImpl implements ValidateService  {
 		}
 		return false;
 	}
+
+	public User authenticate(CredentialsDto credentialsDto) throws NotAuthorizedException {
+        final Optional<CredentialsDto> credentials = Optional.ofNullable(credentialsDto);
+        final String username = credentials.map(CredentialsDto::getUsername).flatMap(Optional::ofNullable).orElse("");
+        final String password = credentials.map(CredentialsDto::getPassword).flatMap(Optional::ofNullable).orElse("");
+        return userRepository.findByCredentialsUsernameAndDeletedFalse(username)
+            .filter(u -> u.getCredentials().getPassword().equals(password))
+            .orElseThrow(() -> new NotAuthorizedException("Invalid User Credentials"));
+    }
 
 }
